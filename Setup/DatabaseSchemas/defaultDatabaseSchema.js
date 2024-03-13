7ad81a010cd8d96663c8958fb354dee65d9a6357
Kushal Ghimire <ghimire.kushal@ymail.com>
2024-03-13 11:11:22 +0545

    FIX: added directory exists check while setting up database

--- Setup/DatabaseSchemas/defaultDatabaseSchema.js
+++ Setup/DatabaseSchemas/defaultDatabaseSchema.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const databasePath = path.resolve(__dirname, '/placeholder/path/to/db');
-const createConsoleLog = require('../../Utils/createConsoleLog')
+
+const createConsoleLog = require('../../Utils/createConsoleLog');
+const ensureDirectoryExists = require('../../Utils/ensureDirectoryExists');
 
 const initializeDatabase = async () => {
+
+  await ensureDirectoryExists(databasePath);
+  
   const db = new sqlite3.Database(databasePath);
 
   db.run('PRAGMA foreign_keys=off;');
